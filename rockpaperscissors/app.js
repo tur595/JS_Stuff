17fe19ec5d996fcddf1d133330beb89f2bccf4e3
Sinan <tur595@hotmail.com>
2021-01-09 15:14:20 +0100

    fixed the attribute for computer-score and added animations

--- rockpaperscissors/app.js
+++ rockpaperscissors/app.js
@@ -20,49 +20,88 @@ const game = () => {
         const options = document.querySelectorAll('.options button');
         const playerHand = document.querySelector('.player-hand');
         const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll('.hands img');
 
-        const computerOptions = ['rock', 'paper', 'scissors'];
+        hands.forEach(hand =>{
+            hand.addEventListener('animationend', function(){
+                this.style.animation = "";
+            });
+        });
 
-        options.forEach(option=>{
-            option.addEventListener('click', function() {
+        const computerOptions = ["rock", "paper", "scissors"];
+
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
 
-                
+                setTimeout(() => {
+                    
+                    compareHands(this.textContent, computerChoice);
+
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+                    
+                }, 2000);
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
             });
         });
     };
 
+    const updateScore = () => {
+        const playerScore = document.querySelector(".player-score p");
+        const computerScore = document.querySelector(".computer-score p");
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    };
+
     const compareHands = (playerChoice, computerChoice) => {
+
         const winner = document.querySelector('.winner');
+
         if(playerChoice === computerChoice){
             winner.textContent = 'It is a tie';
             return;
         }
-        if(playerChoice === 'rock'){
-            if(computerChoice === 'scissors'){
+        if(playerChoice === "rock"){
+            if(computerChoice === "scissors"){
                 winner.textContent = 'Player Wins';
+                pScore ++;
+                updateScore();
                 return;
             }else{
                 winner.textContent = 'Computer Wins';
+                cScore ++;
+                updateScore();
                 return;
             }
         }
-        if(playerChoice === 'scissors'){
-            if(computerChoice === 'paper'){
-                winner.textContent = 'Player Wins';
+        if(playerChoice === "paper"){
+            if(computerChoice === "scissors"){
+                winner.textContent = 'Computer Wins';
+                cScore ++;
+                updateScore();
                 return;
             }else{
-                winner.textContent = 'Computer Wins';
+                winner.textContent = 'Player Wins';
+                pScore ++;
+                updateScore();
                 return;
             }
         }
-        if(playerChoice === 'paper'){
-            if(computerChoice === 'rock'){
-                winner.textContent = 'Player Wins';
+        if(playerChoice === "scissors"){
+            if(computerChoice === "rock"){
+                winner.textContent = "Computer Wins";
+                cScore ++;
+                updateScore();
                 return;
             }else{
-                winner.textContent = 'Computer Wins';
+                winner.textContent = "Player Wins";
+                pScore ++;
+                updateScore();
                 return;
             }
         }
