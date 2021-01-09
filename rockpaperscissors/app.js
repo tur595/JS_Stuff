9840f0bac5528fd8b07d1a46b8e08e94063e7866
Sinan <tur595@hotmail.com>
2021-01-09 13:48:06 +0100

    fixed button behaviour and added opponent choices

--- rockpaperscissors/app.js
+++ rockpaperscissors/app.js
@@ -9,9 +9,34 @@ const game = () => {
 
         playBtn.addEventListener('click', ()=> {
             introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+            setTimeout(() => {
+                match.classList.remove('fadeOut');
+            }, 500);
         });
     };
+
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option=>{
+            option.addEventListener('click', function() {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                console.log(computerChoice);
+            });
+        });
+
+
+
+    }
+
     startGame();
+    playMatch();
 };
 
 game();
