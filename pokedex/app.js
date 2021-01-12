a5e5dc211ac4a4192aab6f8e7701a1533de8c2db
Sinan <tur595@hotmail.com>
2021-01-12 10:21:21 +0100

    implementation of text data fields

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,3 +1,4 @@
+const mainScreen = document.querySelector('.main-screen');
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
 const pokeFrontImage = document.querySelector('.poke-front-image');
@@ -6,3 +7,26 @@ const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        mainScreen.classList.remove('hide');
+        pokeName.textContent = data['name'];
+        pokeId.textContent = data['id'];
+        pokeWeight.textContent = data['weight'];
+        pokeHeight.textContent = data['height'];
+
+        const dataTypes = data['types'];
+        const dataFirstType = dataTypes[0];
+        const dataSecondType = dataTypes[1];
+        pokeTypeOne.textContent = dataFirstType['type']['name'];
+        if(dataSecondType){
+            pokeTypeTwo.classList.remove('hide');
+            pokeTypeTwo.textContent = dataSecondType['type']['name'];
+        } else {
+            pokeTypeTwo.classList.add('hide');
+            pokeTypeTwo.textContent = '';
+        }
+    })
