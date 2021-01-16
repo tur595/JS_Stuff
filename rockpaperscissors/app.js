f682e01250cc83477825a25d69d99e07f35e6ae8
Sinan <tur595@hotmail.com>
2021-01-16 13:25:56 +0100

    added animations and adjusted page behaviour

--- rockpaperscissors/app.js
+++ rockpaperscissors/app.js
@@ -1,34 +1,78 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
 
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const match = document.querySelector('.match');
+        const introScreen = document.querySelector('.intro')
 
-let pScore = 0;
-let cScore = 0;
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+            setTimeout(() => {
+                match.classList.remove('fadeOut');
+            }, 500);
+        })
+    }
 
-const playMatch = () => {
+    const playMatch = () => {
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        const options = document.querySelectorAll('.options button');
+        const hands = document.querySelectorAll('.hands img');
+        const buttons = document.querySelector('.options')
 
-    
-    const playerHand = document.querySelector('.player-hand');
-    const computerHand = document.querySelector('.computer-hand');
-    const options = document.querySelectorAll('.options button');
+        const computerOptions = ['rock', 'paper', 'scissors'];
 
-    const computerOptions = ['rock', 'paper', 'scissors'];
-
-    options.forEach(option => {
-        option.addEventListener("click", function() {
-
-            const computerNumber = Math.floor(Math.random() * 3);
-            const computerChoice = computerOptions[computerNumber];
-
-            playerHand.src = `./assets/${this.textContent}.png`;
-            computerHand.src = `./assets/${computerChoice}.png`;
-
-            compareHands(this.textContent, computerChoice);
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function() {
+                this.style.animation = "";
+            });
         });
-    });
+
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                
+                playerHand.src = `./assets/rock.png`;
+                computerHand.src = `./assets/rock.png`;
+                buttons.classList.add('none');
+                setTimeout(() => {
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+
+                    compareHands(this.textContent, computerChoice);
+                    buttons.classList.remove('none');
+                }, 2000);
+                
+                playerHand.style.animation = "shakePlayer 2s ease"
+                computerHand.style.animation = "shakeComputer 2s ease"
+            });
+        });
+    };
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    }
+
+    // const checkScore = () => {
+    //     const scoreLimit = document.querySelector('.match-nr');
+    //     console.log(scoreLimit.value);
+    //     console.log(pScore);
+    //     console.log(cScore);
+    //     if(pScore === scoreLimit.value || cScore === scoreLimit.value){
+    //         winner.textContent = 'Game over.';
+    //     }
+    // }
 
     const compareHands = (playerChoice, computerChoice) => {
         const winner = document.querySelector('.winner');
-
-
         if(playerChoice === computerChoice){
             winner.textContent = 'It is a tie';
             return;
@@ -74,26 +118,8 @@ const playMatch = () => {
             }
         }
     }
+    playMatch();
+    startGame();
+};
 
-}
-
-const startGame = () => {
-    const playBtn = document.querySelector('.intro button');
-    const match = document.querySelector('.match');
-    const introScreen = document.querySelector('.intro')
-
-    playBtn.addEventListener('click', () => {
-        introScreen.classList.add('fadeOut');
-        match.classList.remove('fadeOut');
-    })
-}
-
-const updateScore = () => {
-    const playerScore = document.querySelector('.player-score p');
-    const computerScore = document.querySelector('.computer-score p');
-    playerScore.textContent = pScore;
-    computerScore.textContent = cScore;
-}
-
-playMatch();
-startGame();
+game();
