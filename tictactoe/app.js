4a80d4b92d3c8dd0b3c82f2a371e8a6dee33e05b
Sinan <tur595@hotmail.com>
2021-01-20 12:57:18 +0100

    added ui behaviour

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -1,1 +1,36 @@
 //html elements
+const statusDiv = document.querySelector('.status')
+const resetDiv = document.querySelector('.reset')
+const cellDivs = document.querySelectorAll('.game-cell')
+
+//variables
+let gameIsLive = true;
+let xIsNext = true;
+
+//event handlers
+const handleReset = (e) => {
+}
+
+const handleCellClick = (e) => {
+    const classList = e.target.classList
+    const location = classList[1]
+
+    if(xIsNext) {
+        e.target.classList.add('x')
+        xIsNext = !xIsNext;
+    }else{
+        e.target.classList.add('o')
+        xIsNext = !xIsNext;
+    }
+
+    if(classList[2] === 'x' || classList[2] === 'o'){
+        e.target.classList.add('used')
+    }
+}
+
+//event listeners
+resetDiv.addEventListener('click', handleReset)
+
+for (const cellDiv of cellDivs) {
+    cellDiv.addEventListener('click', handleCellClick)
+}
